1c280c354e0ccad318ecaa6cf9483133690b98a1
sunnyfixer3941 <sunnyfixer3941@gmail.com>
2019-01-13 12:28:36 +0530

    3rd commit

--- read.py
+++ read.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
 df =  pd.read_csv('datasheet.csv',sep="~")
-df2 = pd.DataFrame(columns=['a','b','c','d','e','f'])
+df2 = pd.DataFrame(columns=['a','b','c','d','e','f','g','h'])
 for index, row in df.iterrows():
+	#Main Algo
     a= row['Area']
     b= row['Yeild']
     c = a*b #crop Amount
     d = (a*3)+(c*2)/5 #Expected Amount
-    e = 100-(a/b*(d-c)/100,2)
+    e = 100-(a/b*(d-c)/100)
     f = c/d
-    g = c/d
-    h = (e+(f*100))/2
-    g = (f*3+(g*100)*1+h*1)/5
-    df2 = pd.Series({'Area':a, 'Yeild':b,'Crop Amount:'c,'Expecting Amount:'d,'Fertility:'e,'Efficiency:'f,'Quality:'h,'Credit Score:'i})
+    g = (e+(f*100))/2
+    h = (e*3+(f*100)*1+g*1)/5
+    df2 = pd.Series({'Area':a, 'Yeild':b,'Crop Amount':c,'Expecting Amount':d,'Fertility':e,'Efficiency':f,'Quality':g,'Credit Score':h})
 
 print(df2)
